d17c525ef3aae30e4b4cc4c5bdb979d3505331fb
Nina Mainusch <nina.mainusch@datadoghq.com>
2023-02-04 14:07:26 +0100

    encoding helpers

---
+++ src/server/helpers.go
@@ -0,0 +1,18 @@
+package server
+
+import (
+	"encoding/binary"
+	"math"
+)
+
+func Float64frombytes(bytes []byte) float64 {
+	bits := binary.BigEndian.Uint64(bytes)
+	float := math.Float64frombits(bits)
+	return float
+}
+
+func float64ToByte(f float64) []byte {
+	var buf [8]byte
+	binary.BigEndian.PutUint64(buf[:], math.Float64bits(f))
+	return buf[:]
+}
